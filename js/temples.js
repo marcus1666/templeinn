1075ef39d84481c8f499ca3621bc401bad54428f
ProNet <myboy1997@gmail.com>
2022-04-06 16:18:33 +0100

    log

--- js/temples.js
+++ js/temples.js
@@ -17,6 +17,7 @@ function makeCard(tempList) {
     });
 
     //temple closure
+    console.log(temple.closure);
     const closures = temple.closure.map((element) => {
       const closureDates = element.dates.map((date) => {
         return `<li>date</li>`;
